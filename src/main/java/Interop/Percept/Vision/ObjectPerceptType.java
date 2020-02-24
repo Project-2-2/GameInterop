da98ab5962344afc51df1dccaa79648ae000db55
Tomasz Darmetko <isinlor@gmail.com>
2020-02-24 21:51:43 +0100

    Add methods tht allow to check properties of object types.

--- src/main/java/Interop/Percept/Vision/ObjectPerceptType.java
+++ src/main/java/Interop/Percept/Vision/ObjectPerceptType.java
@@ -26,4 +26,33 @@ public enum ObjectPerceptType {
         }
     }
 
+    public boolean isSolid() {
+        if(isAgent()) return true;
+        return this == Wall; // only wall is solid
+    }
+
+    public boolean isOpaque() {
+        return isSolid() || isAgent(); // solid objects and agents are opaque
+    }
+
+    public boolean isOpaqueFromOutside() {
+        if(isOpaque()) return true; // opaque objects are opaque from outside
+        switch (this) {
+            case Door:
+            case ShadedArea:
+            case SentryTower:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean isVisibleByGuard() {
+        return this != TargetArea; // only target area not visible by guard
+    }
+
+    public boolean isVisibleByIntruder() {
+        return true; //everything is visible by intruder
+    }
+
 }
