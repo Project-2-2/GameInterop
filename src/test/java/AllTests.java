1f15d95c58bc12d5739eefead9a94c9a7b5f3100
Tomasz Darmetko <isinlor@gmail.com>
2020-02-24 18:39:13 +0100

    Make sure that all tests are run during CI.

--- src/test/java/AllTests.java
+++ src/test/java/AllTests.java
@@ -1,26 +1,53 @@
 import Interop.Agent.Guard;
 import Interop.Agent.Intruder;
 import Interop.InteropTests;
-import Interop.Percept.PerceptsTest;
-import Interop.Utils.UtilsTest;
 import SimpleUnitTest.SimpleUnitTest;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 
 /**
+ * This tests are run by GitHub Actions.
+ *
+ * See: .github/workflows/maven.yml
+ *
  * @author Tomasz Darmetko
  */
 public class AllTests extends SimpleUnitTest {
 
     public static void main(String[] args) {
         InteropTests.main(args);
+        testGroups(args);
         testAgentFactories();
     }
 
+    private static void testGroups(String[] args) {
+        System.out.println("\n\n\nGroup 1 Tests: \n\n");
+        Group1.GroupTests.main(args);
+        System.out.println("\n\n\nGroup 2 Tests: \n\n");
+        Group2.GroupTests.main(args);
+        System.out.println("\n\n\nGroup 3 Tests: \n\n");
+        Group3.GroupTests.main(args);
+        System.out.println("\n\n\nGroup 4 Tests: \n\n");
+        Group4.GroupTests.main(args);
+        System.out.println("\n\n\nGroup 5 Tests: \n\n");
+        Group5.GroupTests.main(args);
+        System.out.println("\n\n\nGroup 6 Tests: \n\n");
+        Group6.GroupTests.main(args);
+        System.out.println("\n\n\nGroup 7 Tests: \n\n");
+        Group7.GroupTests.main(args);
+        System.out.println("\n\n\nGroup 8 Tests: \n\n");
+        Group8.GroupTests.main(args);
+        System.out.println("\n\n\nGroup 9 Tests: \n\n");
+        Group9.GroupTests.main(args);
+        System.out.println("\n\n\nGroup 10 Tests: \n\n");
+        Group10.GroupTests.main(args);
+        System.out.println("\n\n\nGroup 11 Tests: \n\n");
+        Group11.GroupTests.main(args);
+    }
+
     private static void testAgentFactories() {
-        System.out.println("\n\nAgentsFactory:\n");
+        System.out.println("\n\n\nAgentsFactory:\n");
         testAgentsFactory(Group1.AgentsFactory.class);
         testAgentsFactory(Group2.AgentsFactory.class);
         testAgentsFactory(Group3.AgentsFactory.class);
@@ -34,8 +61,7 @@ public class AllTests extends SimpleUnitTest {
         testAgentsFactory(Group11.AgentsFactory.class);
     }
 
-    @SuppressWarnings("rawtypes")
-    public static void testAgentsFactory(Class factoryClass) {
+    public static void testAgentsFactory(Class<?> factoryClass) {
 
         String groupName = factoryClass.getPackage().getName();
         it("allows to build agents of " + groupName, () -> {
@@ -47,8 +73,8 @@ public class AllTests extends SimpleUnitTest {
 
                 Method createIntrudersMethod = factoryClass.getMethod("createIntruders", int.class);
                 Method createGuardsMethod = factoryClass.getMethod("createGuards", int.class);
-                List<Intruder> intruders = (List<Intruder>)createIntrudersMethod.invoke(null, 0);
-                List<Guard> guards = (List<Guard>)createGuardsMethod.invoke(null, 0);
+                createIntrudersMethod.invoke(null, 0);
+                createGuardsMethod.invoke(null, 0);
 
                 executed = true;
 
---
+++ src/test/java/Group2/GroupTests.java
@@ -0,0 +1,25 @@
+package Group2;
+
+import SimpleUnitTest.SimpleUnitTest;
+
+public class GroupTests extends SimpleUnitTest {
+    public static void main(String[] args) {
+        /*
+         *
+         * Here you can write your own tests.
+         *
+         * @see InteropTests for examples of simple unit tests usage.
+         * @see SimpleUnitTest for details on how simple unit tests are implemented.
+         *
+         * Please notice, tests of all groups will be run trough GitHub Actions.
+         * This will allow to verify that all code is working correctly.
+         *
+         * @see AllTests
+         * @see .github/workflows/maven.yml
+         *
+         * In order for you code to be accepted into GameInterop all tests must pass!
+         * You are not required to write any tests, however it is highly recommended!
+         *
+         */
+    }
+}
