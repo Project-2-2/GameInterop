a471c671d61e1938725b00ea00a9397ad015e2ea
vaishnavisirul <32641192+vaishnavisirul@users.noreply.github.com>
2020-03-13 18:13:00 +0100

    objects

---
+++ src/main/java/Group9/gui/Door.java
@@ -0,0 +1,12 @@
+package Group9.gui;
+
+import javafx.scene.paint.Color;
+
+public class Door extends InternalWall {
+	public Door(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
+		super(x1, y1, x2, y2, x3, y3, x4, y4);
+		setFill(Color.BROWN);
+		
+	}
+
+}
