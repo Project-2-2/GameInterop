52fcbb83947250d500f1ca807f738996307fb385
vaishnavisirul <32641192+vaishnavisirul@users.noreply.github.com>
2020-03-14 13:12:39 +0100

    Agent

--- src/main/java/Group9/gui/Agent.java
+++ src/main/java/Group9/gui/Agent.java
@@ -1,57 +1,76 @@
 package Group9.gui;
-import javafx.application.Application;
+
 import javafx.event.EventHandler;
 import javafx.scene.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class Agent 
+public class Agent extends Circle implements GameObject
 {
 	final double x;
 	final double y;
 	final double r;
 	final static double speed = 1.4;
 	final Duration Timestep = Duration.seconds(0.1);
-	
-	public Agent(double x,double y,double r)
+	boolean guard = false;
+	public Agent(double x,double y,double r, boolean guard)
 	{
 		this.x = x;
 		this.y = y;
 		this.r = r;
-		agents(x,y,r);
+		setCenterX(x);
+		setCenterY(y);
+		setRadius(r);
+		if(guard == true)
+		{
+			setFill(Color.RED);
+			setStroke(Color.BLACK);
+		}else 
+		{
+			setFill(Color.BLUE);
+			setStroke(Color.BLACK);
+		}
 		
-	}
-	public static Circle agents(double x, double y, double radius)
-	{
-		Circle agent = new Circle(x,y,radius);
 		
-		return agent;
-				
+		
 	}
 	
-	/*public static void move(Scene scene, Circle agent) {
+	
+	@Override
+	public void updateScale() {
+		// TODO Auto-generated method stub
+		
+		double scale = Scale.scale;
+		setCenterX(x*scale);
+		setCenterY(y*scale);
+		setRadius(r*scale);
+	}
+	
+	public static void move(Scene scene, Agent x) {
 		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
 
 			@Override
 			public void handle(KeyEvent event) {
-				// TODO Auto-generated method stub
+				
 				if(event.getCode()== KeyCode.UP)
-				{agent.setCenterY(agent.getCenterY()-speed);}
+				{x.setCenterY(x.getCenterY()-speed);}
 				else if(event.getCode()== KeyCode.DOWN)
-				{agent.setCenterY(agent.getCenterY()+speed);}
+				{x.setCenterY(x.getCenterY()+speed);}
 				else if(event.getCode()== KeyCode.RIGHT)
-				{agent.setCenterX(agent.getCenterX()+speed);}
+				{x.setCenterX(x.getCenterX()+speed);}
 				else if(event.getCode()== KeyCode.LEFT)
-				{agent.setCenterX(agent.getCenterX()-speed);}
+				{x.setCenterX(x.getCenterX()-speed);}
 				
 			}
 			
 		});
-	}*/
+		
+		
+	}
 	
 	
 	}
