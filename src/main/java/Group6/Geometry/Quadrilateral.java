0e8e53c149dc43561447903c619fbe1e42fa91bc
Tomasz Darmetko <isinlor@gmail.com>
2020-03-14 16:34:54 +0100

    Add explicitly line segments to quadrilateral.

--- src/main/java/Group6/Geometry/Quadrilateral.java
+++ src/main/java/Group6/Geometry/Quadrilateral.java
@@ -1,5 +1,10 @@
 package Group6.Geometry;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Quadrilateral {
 
     private Point pointA;
@@ -7,11 +12,23 @@ public class Quadrilateral {
     private Point pointC;
     private Point pointD;
 
+    private LineSegment AB;
+    private LineSegment BC;
+    private LineSegment CD;
+    private LineSegment DA;
+
     public Quadrilateral(Point pointA, Point pointB, Point pointC, Point pointD) {
+
         this.pointA = pointA;
         this.pointB = pointB;
         this.pointC = pointC;
         this.pointD = pointD;
+
+        AB = new LineSegment(pointA, pointB);
+        BC = new LineSegment(pointB, pointC);
+        CD = new LineSegment(pointC, pointD);
+        DA = new LineSegment(pointD, pointA);
+
     }
 
     public Point getPointA() {
@@ -28,6 +45,40 @@ public class Quadrilateral {
 
     public Point getPointD() {
         return pointD;
+    }
+
+    public LineSegment getAB() {
+        return AB;
+    }
+
+    public LineSegment getBC() {
+        return BC;
+    }
+
+    public LineSegment getCD() {
+        return CD;
+    }
+
+    public LineSegment getDA() {
+        return DA;
+    }
+
+    public List<Point> getAllPoints() {
+        return Arrays.asList(
+            pointA,
+            pointB,
+            pointC,
+            pointD
+        );
+    }
+
+    public List<LineSegment> getAllLineSegments() {
+        return Arrays.asList(
+            AB,
+            BC,
+            CD,
+            DA
+        );
     }
 
 }
