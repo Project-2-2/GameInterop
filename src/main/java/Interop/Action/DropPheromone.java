ca43aea52fd3cf9c851ff73aaa61eb7c1afa4737
Tomasz Darmetko <isinlor@gmail.com>
2020-03-14 13:35:21 +0100

    Add missing pheromone type from drop pheromone action

--- src/main/java/Interop/Action/DropPheromone.java
+++ src/main/java/Interop/Action/DropPheromone.java
@@ -1,9 +1,22 @@
 package Interop.Action;
 
+import Interop.Percept.Smell.SmellPerceptType;
+
 /**
  * This class represents an intention of dropping a pheromone issued by an agent.
  *
  * After a dropping a pheromone an agent will enter a cool down period.
  */
 public final class DropPheromone implements Action, IntruderAction, GuardAction {
+
+    private SmellPerceptType type;
+
+    public DropPheromone(SmellPerceptType type) {
+        this.type = type;
+    }
+
+    public SmellPerceptType getType() {
+        return type;
+    }
+    
 }
