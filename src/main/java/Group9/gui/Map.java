eab134ac1c767c535137c030fa8d337d31a338e4
vaishnavisirul <32641192+vaishnavisirul@users.noreply.github.com>
2020-03-16 20:58:47 +0100

    Update Map.java

--- src/main/java/Group9/gui/Map.java
+++ src/main/java/Group9/gui/Map.java
@@ -95,10 +95,5 @@ public class Map extends Application {
 	    s.show();
 	}
 	
-	public static Polygon draw(double x1,double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
-		Polygon map = new Polygon(x1*Scale.scale,y1*Scale.scale,x2*Scale.scale,y2*Scale.scale,x3*Scale.scale,y3*Scale.scale,x4*Scale.scale,y4*Scale.scale);
-		map.setFill(Color.WHITE);
-	     map.setStroke(Color.BLACK);
-		return map;
-	}
+	
 }
