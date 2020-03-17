1bf632ae2b6c76d9bcd94473ba8dc54b525f3ce2
Jan Krüger <jan@krueger-jan.de>
2020-03-17 12:27:29 +0100

    ups ^^

--- src/main/java/Group9/agent/ExplorerAgent.java
+++ src/main/java/Group9/agent/ExplorerAgent.java
@@ -1,6 +1,8 @@
 package Group9.agent;
 
 import Group9.Game;
+import Group9.math.graph.Graph;
+import Group9.math.graph.Vertex;
 import Interop.Action.GuardAction;
 import Interop.Action.Move;
 import Interop.Action.Rotate;
@@ -9,8 +11,14 @@ import Interop.Geometry.Angle;
 import Interop.Percept.GuardPercepts;
 
 public class ExplorerAgent implements Guard {
+
+    private final Graph<DataContainer> graph = new Graph<>();
+
+    public ExplorerAgent() {}
+
     @Override
     public GuardAction getAction(GuardPercepts percepts) {
+        graph.add(new Vertex<>(new DataContainer(percepts)));
         if(percepts.wasLastActionExecuted())
         {
             System.out.println("move");
@@ -22,6 +30,18 @@ public class ExplorerAgent implements Guard {
             return new Rotate(Angle.fromRadians(
                 percepts.getScenarioGuardPercepts().getScenarioPercepts().getMaxRotationAngle().getRadians() * Game._RANDOM.nextDouble()
             ));
+        }
+    }
+
+    public static class DataContainer {
+        private GuardPercepts percepts;
+        public DataContainer(GuardPercepts percepts)
+        {
+            this.percepts = percepts;
+        }
+
+        public GuardPercepts getPercepts() {
+            return percepts;
         }
     }
 }
--- src/main/java/Group9/agent/container/ExplorerContainer.java
+++ src/main/java/Group9/agent/container/ExplorerContainer.java
@@ -1,13 +1,10 @@
 package Group9.agent.container;
 
-import Group9.math.graph.Graph;
 import Interop.Agent.Guard;
 import Interop.Geometry.Vector;
 import Interop.Percept.Vision.FieldOfView;
 
 public class ExplorerContainer extends AgentContainer<Guard> {
-
-    private final Graph<AgentContainer.DataContainer> graph = new Graph<>();
 
     public ExplorerContainer(Guard agent, Vector position, Vector direction, FieldOfView normalFOV) {
         super(agent, position, direction, normalFOV);
