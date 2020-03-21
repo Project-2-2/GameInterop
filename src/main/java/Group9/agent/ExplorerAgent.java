5c37001be0326eaf01d8f5b0151ed2e34520fddb
Jan Krüger <jan@krueger-jan.de>
2020-03-21 17:21:00 +0100

    drop test pheremones

--- src/main/java/Group9/agent/ExplorerAgent.java
+++ src/main/java/Group9/agent/ExplorerAgent.java
@@ -3,12 +3,14 @@ package Group9.agent;
 import Group9.Game;
 import Group9.math.graph.Graph;
 import Group9.math.graph.Vertex;
+import Interop.Action.DropPheromone;
 import Interop.Action.GuardAction;
 import Interop.Action.Move;
 import Interop.Action.Rotate;
 import Interop.Agent.Guard;
 import Interop.Geometry.Angle;
 import Interop.Percept.GuardPercepts;
+import Interop.Percept.Smell.SmellPerceptType;
 import Interop.Percept.Vision.ObjectPerceptType;
 
 public class ExplorerAgent implements Guard {
@@ -20,6 +22,13 @@ public class ExplorerAgent implements Guard {
     @Override
     public GuardAction getAction(GuardPercepts percepts) {
         graph.add(new Vertex<>(new DataContainer(percepts)));
+
+        if(Math.random() < 0.1)
+        {
+            System.out.println("drop pheremone");
+            return new DropPheromone(SmellPerceptType.Pheromone1);
+        }
+
         if(percepts.wasLastActionExecuted())
         {
             return new Move(percepts.getScenarioGuardPercepts().getMaxMoveDistanceGuard());
