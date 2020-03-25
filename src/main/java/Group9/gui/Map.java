acce467df4cb12a7484869b1ee227d3e798408d7
Merijn <vanmoorsel.merijn@gmail.com>
2020-03-25 13:56:48 +0100

    Update Map.java

--- src/main/java/Group9/gui/Map.java
+++ src/main/java/Group9/gui/Map.java
@@ -47,13 +47,7 @@ public class Map extends Application {
 		Group root = new Group();
 
 		Scene scene = new Scene(root, 970, 630,Color.BURLYWOOD);
-
-		s.setScene(scene);
-		s.setMaximized(true);
-		System.out.println("max height: " + s.getMaxHeight());
-		s.setTitle("Map ");
-		s.setResizable(true);
-		s.show();
+		
 
 	    root.getChildren().addAll(map, staticObjects, movingObjects, new Legend(s.getWidth()-150, 30));
 
