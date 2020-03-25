59ee98d6c97c3131460da2afe949a167a273beab
Greendale <stijnvdgroenendaal@gmail.com>
2020-03-25 18:47:34 +0100

    Added color to map objects

--- src/main/java/Group6/GUI/Target.java
+++ src/main/java/Group6/GUI/Target.java
@@ -1,7 +1,10 @@
 package Group6.GUI;
 
+import javafx.scene.paint.Color;
+
 public class Target extends Wall {
     public Target(double[] x, double[] y){
         super(x,y);
+        setFill(Color.SEAGREEN);
     }
 }
