82e8a3b4380d54dc781bbcda4baf51b07dccc263
Merijn <vanmoorsel.merijn@gmail.com>
2020-04-16 10:34:19 +0200

    starting on the grid implementation for rttes

---
+++ src/main/java/Group9/agent/Intruder/Intruder1.java
@@ -0,0 +1,20 @@
+package Group9.agent.Intruder;
+
+import Group9.agent.container.IntruderContainer;
+import Group9.math.Vector2;
+import Interop.Action.IntruderAction;
+import Interop.Agent.Intruder;
+import Interop.Percept.IntruderPercepts;
+import Interop.Percept.Vision.FieldOfView;
+
+public class Intruder1 implements Intruder {
+    public Intruder1()
+    {
+
+    }
+
+    @Override
+    public IntruderAction getAction(IntruderPercepts percepts) {
+        return null;
+    }
+}
