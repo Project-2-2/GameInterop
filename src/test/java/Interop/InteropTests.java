0dd33ca734445f225320c2e2858efcda20583b1e
vko <vladislav.kolev@kex-ag.com>
2020-04-17 01:03:33 +0300

    uncomment code

--- src/test/java/Interop/InteropTests.java
+++ src/test/java/Interop/InteropTests.java
@@ -1,16 +1,16 @@
-//package Interop;
-//
-//import Interop.Geometry.DirectionTest;
-//import Interop.Percept.PerceptsTest;
-//import Interop.Utils.UtilsTest;
-//
-///**
-// * @author Tomasz Darmetko
-// */
-//public class InteropTests {
-//    public static void main(String[] args) {
-//        UtilsTest.main(args);
-//        DirectionTest.main(args);
-//        PerceptsTest.main(args);
-//    }
-//}
+package Interop;
+
+import Interop.Geometry.DirectionTest;
+import Interop.Percept.PerceptsTest;
+import Interop.Utils.UtilsTest;
+
+/**
+ * @author Tomasz Darmetko
+ */
+public class InteropTests {
+    public static void main(String[] args) {
+        UtilsTest.main(args);
+        DirectionTest.main(args);
+        PerceptsTest.main(args);
+    }
+}
