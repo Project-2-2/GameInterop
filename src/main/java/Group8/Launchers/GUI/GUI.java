3b234d55a26d193362585df463fd65bb0195a59d
Lucsparidans <lucsnadiraps@gmail.com>
2020-05-13 10:48:49 +0200

    Layers and movement

--- src/main/java/Group8/Launchers/GUI/GUI.java
+++ src/main/java/Group8/Launchers/GUI/GUI.java
@@ -8,7 +8,6 @@ import Group9.map.dynamic.DynamicObject;
 import Group9.map.parser.Parser;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -17,13 +16,16 @@ import java.util.List;
 
 
 public class GUI extends Application{
+    private final int WIDTH = 800, HEIGHT = 600;
+
     //If we need to pass a specific factory make sure to use -1 for ticks since that will be the fastest
     private final Game game = new Game(Parser.parseFile("./src/main/java/Group9/map/maps/test_2.map"),false);
 
-    private final GameScene scene = new GameScene(new StackPane());
+    private final GameScene scene = new GameScene(new StackPane(),WIDTH,HEIGHT);
 
     private AnimationTimer timer;
 
+    private MovableObject obj = new MovableObject(0,0,50,50);
 
     /**
      * Launches the GUI
@@ -35,9 +37,9 @@ public class GUI extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setHeight(600);
-        stage.setWidth(800);
-        stage.setTitle("G8");
+        stage.setHeight(HEIGHT);
+        stage.setWidth(WIDTH);
+        stage.setTitle("Group 8");
         stage.setScene(scene);
         stage.show();
         stage.setOnCloseRequest(windowEvent -> {
@@ -57,10 +59,63 @@ public class GUI extends Application{
         timer.start();
     }
 
-
     private void drawMovables(List<GuardContainer> guards, List<IntruderContainer> intruders, List<DynamicObject<?>> objects){
         //Implement this somehow....
         scene.drawRect();
+        scene.drawMovableObject(obj);
+        obj.move(1,1);
+
+    }
+}
+
+class MovableObject{
+    private double x;
+    private double y;
+    private int width,height;
+
+    public MovableObject(double x, double y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void move(double x, double y){
+        this.x += x;
+        this.y += y;
+        System.out.println(String.format("New location at x: %f & y: %f",this.x,this.y));
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
 
--- src/main/java/Group8/Launchers/GUI/GameScene.java
+++ src/main/java/Group8/Launchers/GUI/GameScene.java
@@ -8,30 +8,57 @@ import javafx.scene.paint.Color;
 
 public class GameScene extends Scene {
 
-    private Canvas canvas;
-    private GraphicsContext gc;
+    private final int WIDTH,HEIGHT;
+
+    private Canvas background;
+    private Canvas foreground;
+
+    private GraphicsContext gcBackground;
+    private GraphicsContext gcForeground;
     private StackPane parent;
 
-    public GameScene(StackPane parent) {
+    public GameScene(StackPane parent, int width, int height) {
         super(parent);
+        WIDTH = width;
+        HEIGHT = height;
         init(parent); // Initializes the needed variables
         constructScene(); // Necessary setup for the scene
 
     }
 
     private void constructScene(){
-        parent.getChildren().add(canvas);
+        parent.getChildren().add(background);
+        parent.getChildren().add(foreground);
+
+        drawBackgroundLayer();
     }
 
+
     private void init(StackPane parent){
-        canvas = new Canvas(800,600);
-        gc = canvas.getGraphicsContext2D();
+        background = new Canvas(WIDTH,HEIGHT);
+        foreground = new Canvas(WIDTH,HEIGHT);
+        gcBackground = background.getGraphicsContext2D();
+        gcForeground = foreground.getGraphicsContext2D();
         this.parent = parent;
     }
 
     public void drawRect(){
-        gc.setFill(Color.BLUE);
-        gc.fillRect(100,100,50,50);
+        gcBackground.setFill(Color.BLUE);
+        gcBackground.fillRect(0,0,WIDTH,HEIGHT);
+    }
+
+    public void drawMovableObject(MovableObject o){
+        System.out.println("Clearing foreground");
+        gcForeground.clearRect(0,0,WIDTH,HEIGHT);
+        System.out.println("Draw foreground");
+        gcForeground.setFill(Color.RED);
+        gcForeground.fillRect(o.getX(),o.getY(),o.getWidth(),o.getHeight());
+    }
+
+    private void drawBackgroundLayer(){
+        System.out.println("Draw background");
+        gcBackground.clearRect(0,0,WIDTH,HEIGHT); // Clear the screen
+        drawRect();
     }
 
 
