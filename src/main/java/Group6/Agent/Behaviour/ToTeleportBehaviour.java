010f33e8d94b9559d23f57d23205ba3a0f0d7a07
Tomasz Darmetko <isinlor@gmail.com>
2020-05-13 21:12:29 +0200

    Add documentation of ToTeleport

--- src/main/java/Group6/Agent/Behaviour/ToTeleportBehaviour.java
+++ src/main/java/Group6/Agent/Behaviour/ToTeleportBehaviour.java
@@ -8,6 +8,8 @@ import Interop.Percept.Percepts;
 import Interop.Percept.Vision.ObjectPercepts;
 
 /**
+ * This behaviour will lead agent toward teleport.
+ * Soon after teleportation agent will avoid teleports in order to explore the new place.
  * @author Tomasz Darmetko
  */
 public class ToTeleportBehaviour implements Behaviour {
@@ -18,22 +20,24 @@ public class ToTeleportBehaviour implements Behaviour {
     public Action getAction(Percepts percepts) {
         ObjectPercepts teleportPercepts = PerceptsService.getTeleportPercepts(percepts);
         double towardsTeleport = PerceptsService.getMeanClockDirection(teleportPercepts) - 180;
+        // turn away from a teleport if just teleported - provides time for exploration
         if(tillNextTeleport > 0) towardsTeleport = towardsTeleport * -1.0;
-        if(tillNextRotation == 0) tillNextRotation = 5;
+        if(tillNextRotation == 0) tillNextRotation = 5; // allows to avoid constant rotating
         return ActionsFactory.getValidRotate(towardsTeleport, percepts);
     }
 
     public boolean shouldExecute(Percepts percepts) {
         if(percepts.getAreaPercepts().isJustTeleported()) return false;
         if(PerceptsService.getTeleportPercepts(percepts).getAll().isEmpty()) return false;
-        if(tillNextTeleport > 0) return true;
-        if(tillNextRotation > 0) return false;
+        if(tillNextTeleport > 0) return true; // allows to avoid teleports soon after teleporting
+        if(tillNextRotation > 0) return false; // allows to avoid constant rotation
         return true;
     }
 
     public void updateState(Percepts percepts) {
         if(tillNextTeleport > 0) tillNextTeleport--;
         if(tillNextRotation > 0) tillNextRotation--;
+        // provides time for exploration of the new place
         if(percepts.getAreaPercepts().isJustTeleported()) tillNextTeleport = 1000;
     }
 
