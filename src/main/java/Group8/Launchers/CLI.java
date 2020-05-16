65a40fb90f4d01d49b6fc3f23a51d7764545e0c9
Lucsparidans <lucsnadiraps@gmail.com>
2020-05-16 09:41:59 +0200

    CLI

--- src/main/java/Group8/Launchers/CLI.java
+++ src/main/java/Group8/Launchers/CLI.java
@@ -1,4 +1,9 @@
 package Group8.Launchers;
 
 public class CLI {
+    public static void main(String[] args) {
+        // TODO: Create a game object
+        // TODO: Create a Thread that takes the game object as an argument
+        // TODO: Run the created thread
+    }
 }
