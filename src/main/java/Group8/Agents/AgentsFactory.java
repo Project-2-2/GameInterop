c579d032c537804988767bd715271cfae5285cca
tobias <merschtobias@gmx.net>
2020-05-16 21:17:26 +0200

    added AgentsFactory

--- src/main/java/Group8/Agents/AgentsFactory.java
+++ src/main/java/Group8/Agents/AgentsFactory.java
@@ -1,5 +1,6 @@
 package Interop.Agent;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,26 +14,21 @@ import java.util.List;
  */
 public class AgentsFactory {
 
-    static public enum AlgoI {
+    public enum Algo {
         AI1,
         AI2;
     }
 
-    static public enum AlgoG {
-        AI1,
-        AI2;
-    }
-
-    static public List<Intruder> createIntruders(int number, AlgoI whichAlgo) {
+    static public List<Intruder> createIntruders(int number, Algo whichAlgo) {
         List<Intruder> intruders = new ArrayList<>();
 
-        for(i=0; i<number; i++){
+        for(int i=0; i<number; i++){
             switch(whichAlgo) {
-                case (AlgoI.AI1):
-                    intruders.add(new IntruAlgo1());
+                case (AI1):
+                    //intruders.add(new IntruAlgo1());
                     break;
-                case (AlgoI.AI2):
-                    intruders.add(new IntruAlgo2());
+                case (AI2):
+                    //intruders.add(new IntruAlgo2());
                     break;
             }
         }
@@ -40,16 +36,16 @@ public class AgentsFactory {
         return intruders;
     }
 
-    static public List<Guard> createGuards(int number, AlgoG whichAlgo) {
+    static public List<Guard> createGuards(int number, Algo whichAlgo) {
         List<Guard> guards = new ArrayList<>();
 
-        for(i=0; i<number; i++){
+        for(int i=0; i<number; i++){
             switch(whichAlgo) {
-                case (AlgoG.AI1):
-                    guards.add(new GuardAlgo1());
+                case (AI1):
+                    //guards.add(new GuardAlgo1());
                     break;
-                case (AlgoG.AI2):
-                    guards.add(new GuardAlgo2());
+                case (AI2):
+                    //guards.add(new GuardAlgo2());
                     break;
             }
         }
