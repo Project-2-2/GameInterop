65e48453bcfa99aac23654dccb71d98a25f15d17
Lucsparidans <33491773+Lucsparidans@users.noreply.github.com>
2020-05-18 16:41:35 +0200

    Cleaning up

--- src/main/java/Group8/Launchers/CLI.java
+++ src/main/java/Group8/Launchers/CLI.java
@@ -1,9 +1,16 @@
 package Group8.Launchers;
 
+import Group9.Game;
+import Group9.agent.factories.DefaultAgentFactory;
+import Group9.map.parser.Parser;
+
 public class CLI {
     public static void main(String[] args) {
         // TODO: Create a game object
         // TODO: Create a Thread that takes the game object as an argument
         // TODO: Run the created thread
+        Game game = new Game(Parser.parseFile("./src/main/java/Group9/map/maps/TestMapEasy.map"), new DefaultAgentFactory(), false);
+        game.run();
+        System.out.printf("The winner is: %s\n", game.getWinner());
     }
 }
--- src/main/java/Group8/PathFinding/SimplePathfinding.java
+++ src/main/java/Group8/PathFinding/SimplePathfinding.java
@@ -1,5 +1,6 @@
 package Group8.PathFinding;
 
+import Group9.Game;
 import Interop.Action.IntruderAction;
 import Interop.Action.Move;
 import Interop.Action.Rotate;
@@ -10,30 +11,42 @@ import Interop.Percept.Scenario.SlowDownModifiers;
 import Interop.Utils.Utils;
 
 import static java.lang.Double.NaN;
+import static java.lang.Double.isNaN;
 
 
 public class SimplePathfinding {
 
     private final Angle MAX_ROTATION;
+    private final double EPS = 1e-6;
 
+    /*
+    * The angle is never set directly towards the goal since this produces a NaN value when generating intruderpercepts
+    * The deviation is a a way to make sure it is never exactly 0
+    */
     public SimplePathfinding(IntruderPercepts percepts) {
         MAX_ROTATION = percepts.getScenarioIntruderPercepts().getScenarioPercepts().getMaxRotationAngle();
-        System.out.println(MAX_ROTATION.getDegrees());
     }
 
     public IntruderAction getMoveIntruder(IntruderPercepts percepts) {
-        System.out.println(percepts.getTargetDirection().getRadians());
-        if((Math.abs(percepts.getTargetDirection().getRadians())) <= 0.001){
+        if((Math.abs(percepts.getTargetDirection().getRadians())) <= EPS){
             return new Move(new Distance(percepts.getScenarioIntruderPercepts().getMaxMoveDistanceIntruder().getValue() * getSpeedModifier(percepts)));
 
         }
-        if(percepts.getTargetDirection().getRadians() >= MAX_ROTATION.getRadians()){
+        if(percepts.getTargetDirection().getRadians() > MAX_ROTATION.getRadians()){
             //System.out.println("io");
             return new Rotate(Angle.fromRadians(MAX_ROTATION.getRadians()));
         }else{
             //System.out.println(Utils.isRealNumber(percepts.getTargetDirection().getRadians()));
             //System.out.println(String.format("Angle is real: %b, number: %f (in rad)",Utils.isRealNumber(percepts.getTargetDirection().getRadians()),percepts.getTargetDirection().getRadians()));
-            return new Rotate(Angle.fromRadians(percepts.getTargetDirection().getRadians()+0.0001));
+            return new Rotate(Angle.fromRadians(percepts.getTargetDirection().getRadians() + deviation()));
+        }
+    }
+
+    private double deviation(){
+        if(Game._RANDOM.nextBoolean()) {
+            return Game._RANDOM.nextDouble() * EPS;
+        }else{
+            return -Game._RANDOM.nextDouble() * EPS;
         }
     }
 
