86f57c2990214f1111a9362396d6d1e54ca95be4
Tomasz Darmetko <isinlor@gmail.com>
2020-06-15 15:52:04 +0200

    Add AgentsFactory

--- src/main/java/Group6/AgentsFactory.java
+++ src/main/java/Group6/AgentsFactory.java
@@ -1,8 +1,13 @@
 package Group6;
 
+import Group6.Agent.Behaviour.*;
+import Group6.Agent.Guard.BehaviourBasedGuard;
+import Group6.Agent.Intruder.BehaviourBasedIntruder;
 import Interop.Agent.Guard;
 import Interop.Agent.Intruder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,10 +20,33 @@ import java.util.List;
  * Agents must not hold ANY references to common objects or references to each other.
  */
 public class AgentsFactory {
-    static public List<Intruder> createIntruders(int number) {
-        return Collections.emptyList();
+    
+    static public List<Intruder> createIntruders(int amount) {
+        List<Intruder> list = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            list.add(new BehaviourBasedIntruder(Arrays.asList(
+                new ToTargetBehaviour(),
+                new ToTeleportBehaviour(),
+                new ToPassageBehaviour(),
+                new AvoidWallsBehaviour(),
+                new DisperseBehaviour(),
+                new ExploreBehaviour()
+            )));
+        }
+        return list;
     }
-    static public List<Guard> createGuards(int number) {
-        return Collections.emptyList();
+
+    static public List<Guard> createGuards(int amount) {
+        List<Guard> list = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            list.add(new BehaviourBasedGuard(Arrays.asList(
+                new YellBehaviour(),
+                new FollowIntruderBehaviour(),
+                new FollowYellBehaviour(),
+                new ExploreBehaviour()
+            )));
+        }
+        return list;
     }
+
 }
