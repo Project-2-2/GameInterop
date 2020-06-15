55a63373ab0a38955d7f4fb75b2efe19cb8c4d7e
camillebouvy <bouvy2000@gmail.com>
2020-06-15 19:09:05 +0200

    Group 2 agents

--- src/main/java/Group2/AgentsFactory.java
+++ src/main/java/Group2/AgentsFactory.java
@@ -3,22 +3,33 @@ package Group2;
 import Interop.Agent.Guard;
 import Interop.Agent.Intruder;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * This class provides common way to build agents for the competition.
- * 
+ *
  * Sharing knowledge between agents is NOT ALLOWED.
  *
  * For example:
  * Agents must not hold ANY references to common objects or references to each other.
  */
 public class AgentsFactory {
-    static public List<Intruder> createIntruders(int number) {
-        return Collections.emptyList();
+
+    public static List<Intruder> createIntruders(int number) {
+        ArrayList<Intruder> intruders = new ArrayList<>();
+        for(int i=0;i<number;i++){
+            Intruder intruder = new GreedyIntruderAgent(i);
+            intruders.add(intruder);
+        }
+        return intruders;
     }
-    static public List<Guard> createGuards(int number) {
-        return Collections.emptyList();
+    public static List<Guard> createGuards(int number) {
+        ArrayList<Guard> guards = new ArrayList<>();
+        for(int i=0;i<number;i++){
+            Guard guard = new GuardAgent1(i);
+            guards.add(guard);
+        }
+        return guards;
     }
 }
