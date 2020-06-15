9b7697792f19277c5f9b2d3023e8eca7e374c854
Meelzak <43891745+Meelzak@users.noreply.github.com>
2020-06-15 17:06:05 +0200

    fourth try

--- src/main/java/Group5/Main.java
+++ src/main/java/Group5/Main.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.fxml.*;
 
 public class Main extends Application {
 
