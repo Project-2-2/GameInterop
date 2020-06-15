74d4d1fe206f2a791292e139a0d2c289d4b89940
Tomasz Darmetko <isinlor@gmail.com>
2020-06-15 12:59:25 +0200

    Fix issues with JavaFx deps.

--- src/main/java/Group6/GUI/Yell.java
+++ src/main/java/Group6/GUI/Yell.java
@@ -3,9 +3,9 @@ package Group6.GUI;
 import Group6.Geometry.Point;
 import javafx.scene.paint.Color;
 
-public class Yell extends Dot{
+public class Yell {
     public Yell(Point center, double radius) {
-        super(center, radius);
-        setFill(Color.MAGENTA);
+//        super(center, radius);
+//        setFill(Color.MAGENTA);
     }
 }
