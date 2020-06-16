ade040121f635239c8ca7a1862ff2f7e2ba83da6
Tomasz Darmetko <isinlor@gmail.com>
2020-06-16 12:45:58 +0200

    Revert "Add Group 4 code."
    This reverts commit 13ad14fdd02ee75778e6b043dacb032af37037cd.

--- src/main/java/Group4/AgentsFactory.java
+++ src/main/java/Group4/AgentsFactory.java
@@ -1,14 +1,9 @@
 package Group4;
 
-import Group4.Guards.PatrolGuard;
-import Group4.Guards.FollowGuard;
-import Group4.Intruder.OurIntruder;
 import Interop.Agent.Guard;
 import Interop.Agent.Intruder;
-import Group9.agent.factories.*;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -19,22 +14,11 @@ import java.util.List;
  * For example:
  * Agents must not hold ANY references to common objects or references to each other.
  */
-public class AgentsFactory implements IAgentFactory{
-
-    //minor change
-    public List<Intruder> createIntruders(int number) {
-        List<Intruder> intruders = new LinkedList<>();
-        for(int i = 0; i < number; i++){
-            intruders.add(new OurIntruder());
-        }
-        return intruders;
+public class AgentsFactory {
+    static public List<Intruder> createIntruders(int number) {
+        return Collections.emptyList();
     }
-    public List<Guard> createGuards(int number) {
-        List<Guard> guards = new ArrayList<>();
-        for(int i = 0; i < number; i++)
-        {
-            guards.add(new FollowGuard());
-        }
-        return guards;
+    static public List<Guard> createGuards(int number) {
+        return Collections.emptyList();
     }
 }
