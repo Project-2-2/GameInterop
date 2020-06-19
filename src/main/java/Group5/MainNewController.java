76174046486589c7eaf2b02b0f7a84c3c7939360
Meelzak <43891745+Meelzak@users.noreply.github.com>
2020-06-19 13:05:46 +0200

    setup for experiments

--- src/main/java/Group5/MainNewController.java
+++ src/main/java/Group5/MainNewController.java
@@ -19,7 +19,7 @@ public class MainNewController {
 //    }
 
     public static void main(String[] args) {
-        int epochs = 1000;
+        int epochs = 100;
         int guardWins = 0;
         for (int i=0; i<epochs; i++){
             Game game = new Game(Parser.parseFile("./src/main/java/Group9/map/maps/test_2.map"), new AgentFactoryGroup5(), false);
